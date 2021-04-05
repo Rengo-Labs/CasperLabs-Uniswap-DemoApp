8363daf1165470a476bd87b170ffb1e73a8fc7aa
adreees0512133526 <muhammad.adrees@scytalelabs.com>
2021-04-05 13:43:37 +0500

    Create Nft Updated

--- src/app/containers/Pages/Dashboard/Admin/NewNFT.js
+++ src/app/containers/Pages/Dashboard/Admin/NewNFT.js
@@ -7,6 +7,7 @@ import Button from '@material-ui/core/Button';
 import CardActions from '@material-ui/core/CardActions';
 import TextField from "@material-ui/core/TextField";
 import Typography from '@material-ui/core/Typography';
+
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import axios from "axios";
 import Cookies from "js-cookie";
@@ -21,10 +22,13 @@ import r3 from '../../../../assets/img/r3.jpg';
 import r4 from '../../../../assets/img/r4.jpg';
 import r5 from '../../../../assets/img/r5.jpg';
 import r6 from '../../../../assets/img/r6.jpg';
+import audio from '../../../../assets/mp3/music.mp3';
 
 
 import ReactDOM from "react-dom";
 import Cube from "react-3d-cube";
+
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -56,17 +60,69 @@ const useStyles = makeStyles((theme) => ({
 
 
 function NewNFT(props) {
+    // const  = ;
+
+    // variable to play audio in loop
+    const [audioTune, setAudioTune] = useState(new Audio(audio));
+    const [playInLoop, setPlayInLoop] = useState(false);
+
+    // load audio file on component load
+    useEffect(() => {
+        audioTune.load();
+    }, [])
+
+    // set the loop of audio tune
+    useEffect(() => {
+        audioTune.loop = playInLoop;
+    }, [playInLoop])
+
+    // play audio sound
+    const playSound = () => {
+        var playPromise = audioTune.play();
+        console.log("audioTune", audioTune);
+
+        // if (playPromise !== undefined) {
+        //     playPromise
+        //         .then(_ => {
+        //             // Automatic playback started!
+        //             // Show playing UI.
+        //             console.log(audio);
+        //             console.log("audio played auto");
+        //         })
+        //         .catch(error => {
+        //             // Auto-play was prevented
+        //             // Show paused UI.
+        //             console.log("playback prevented", error);
+        //         });
+        // }
+        audioTune.play();
+    }
+
+    // pause audio sound
+    const pauseSound = () => {
+        console.log("audioTune", audioTune);
+        audioTune.pause();
+    }
+
+    // stop audio sound
+    const stopSound = () => {
+        console.log("audioTune", audioTune);
+        audioTune.pause();
+        audioTune.currentTime = 0;
+    }
     const { enqueueSnackbar } = useSnackbar();
     const classes = useStyles();
-    const [inputList, setInputList] = useState([]);
+    // const [inputList, setInputList] = useState([]);
     let [isSaving, setIsSaving] = useState(false);
-    let [category, setCategory] = useState();
+    let [title, setTitle] = useState();
     let [description, setDescription] = useState();
-    let [importers, setImporters] = useState();
+    let [types, setTypes] = useState(["Common", "Rare", "Epic", "Lgendary", "Uncommon"]);
+    let [supplies, setSupplies] = useState(["Fixed Supply", "Variable Supply", "Mintable"]);
+    let [supply, setSupply] = useState("");
     let [fileData, setFileData] = useState('');
-    let [tokens, setTokens] = useState();
+    let [tokenSupply, setTokenSupply] = useState();
     let [importerName, setImporterName] = useState('');
-    let [importerId, setImporterId] = useState();
+    let [type, setType] = useState();
     let [isDisabledImporter, setIsDisabledImporter] = useState(true);
     let [image1Blob, setImage1Blob] = useState(r1);
     let [image2Blob, setImage2Blob] = useState(r1);
@@ -81,26 +137,20 @@ function NewNFT(props) {
     let [image4, setImage4] = useState(r1);
     let [image5, setImage5] = useState(r1);
     let [image6, setImage6] = useState(r1);
+    let [music, setMusic] = useState();
 
     let fileSelectHandler = (event, index) => {
         if (event.target.files[0] !== undefined) {
             setFileData(event.target.files[0]);
         }
     };
-
-
-    let getImporters = () => {
-        axios
-            .get("/api/v1/exporter/getAllImporters", { withCredentials: true })
-            .then((response) => {
-                console.log("response", response);
-                setImporters(response.data.importers);
-                setIsDisabledImporter(false);
-            })
-            .catch((error) => {
-                console.log(error);
-                console.log(error.response);
-            });
+    let uploadMusicHandler = (event, index) => {
+        console.log("event.target.files", event.target.files);
+        console.log("event.target.value", event.target.value);
+        if (event.target.files[0] !== undefined) {
+            setMusic(event.target.files[0]);
+            setAudioTune(event.target.value);
+        }
     };
 
     useEffect(() => {
@@ -113,7 +163,6 @@ function NewNFT(props) {
             termsandconditions: "",
             changePassword: "",
         });
-        getImporters();// eslint-disable-next-line
     }, []);
 
     const handleSubmitEvent = (event) => {
@@ -123,23 +172,22 @@ function NewNFT(props) {
         let jwt = Cookies.get("Authorization");
         let jwtDecoded = jwtDecode(jwt);
         let exporter = jwtDecoded.id;
-        let importer = importerId;
         let fileData = new FormData();
-        fileData.append("importerId", importer);
+        // fileData.append("importerId", importer);
         fileData.append("exporterId", exporter);
-        console.log("JSON.stringify(inputList)", inputList);
+        // console.log("JSON.stringify(inputList)", inputList);
         let catagoryArray = [];
         let descriptionArray = [];
-        for (let i = 0; i < inputList.length; i++) {
-            catagoryArray.push(inputList[i].category);
-            descriptionArray.push(inputList[i].description);
-            fileData.append(`file`, inputList[i].fileData);
-        }
+        // for (let i = 0; i < inputList.length; i++) {
+        //     catagoryArray.push(inputList[i].title);
+        //     descriptionArray.push(inputList[i].description);
+        //     fileData.append(`file`, inputList[i].fileData);
+        // }
         console.log(descriptionArray);
 
         fileData.append(`description`, JSON.stringify(descriptionArray));
         fileData.append(`documentNames`, JSON.stringify(catagoryArray));
-        fileData.append(`numberOfTokens`, tokens * 10 ** 18);
+        fileData.append(`numberOfTokens`, tokenSupply * 10 ** 18);
 
         for (var pair of fileData.entries()) {
             console.log(pair[0] + ', ' + pair[1]);
@@ -163,19 +211,19 @@ function NewNFT(props) {
             }
         );
     };
-    const handleRemoveClick = (index) => {
-        const list = [...inputList];
-        list.splice(index, 1);
-        setInputList(list);
-    };
+    // const handleRemoveClick = (index) => {
+    //     const list = [...inputList];
+    //     list.splice(index, 1);
+    //     setInputList(list);
+    // };
 
     // handle click event of the Add button
     const handleAddClick = () => {
 
-        setInputList([...inputList, { category: category, description: description, fileData: fileData }]);
-        setCategory('');
-        setDescription('');
-        setFileData('');
+        // setInputList([...inputList, { title: title, description: description, fileData: fileData }]);
+        // setTitle('');
+        // setDescription('');
+        // setFileData('');
     };
 
     let onChangeFile = (e) => {
@@ -274,6 +322,14 @@ function NewNFT(props) {
             <div className="card-body">
                 <div className="row">
                     <div className="col-md-12 col-lg-6">
+                        {/* <div className="App">
+                            <audio src={audioTune} />
+                            
+                            <input type="button" className="btn btn-primary mr-2" value="Play" onClick={playSound}></input>
+                            <input type="button" className="btn btn-warning mr-2" value="Pause" onClick={pauseSound}></input>
+                            <input type="button" className="btn btn-danger mr-2" value="Stop" onClick={stopSound}></input>
+
+                        </div> */}
                         <form onSubmit={handleSubmitEvent}>
                             <div className="form-group">
                                 <label>Select Artwork</label>
@@ -284,7 +340,7 @@ function NewNFT(props) {
                                             <div className="upload-img">
                                                 <div
                                                     className="change-photo-btn"
-                                                    style={{ backgroundColor: "rgb(167,0,0)" }}
+                                                    // style={{ backgroundColor: "rgb(167,0,0)" }}
                                                 >
                                                     <span>
                                                         <i className="fa fa-upload"></i>
@@ -307,37 +363,92 @@ function NewNFT(props) {
                                         </div>
                                     </div>
                                 </div>
+                                <label>Select Type</label>
+                                <div className="filter-widget">
+                                    <Autocomplete
+                                        id="combo-dox-demo"
+                                        required
+                                        options={types}
+                                        // disabled={isDisabledImporter}
+                                        getOptionLabel={(option) =>
+                                            option
+                                        }
+                                        onChange={(event, value) => {
+                                            if (value == null) setType("");
+                                            else {
+                                                console.log(value);
+                                                setType(value)
+                                            }
+                                        }}
+                                        renderInput={(params) => (
+                                            <TextField
+                                                {...params}
+                                                label="Type"
+                                                variant="outlined"
+                                            />
+                                        )}
+                                    />
+                                </div>
+
                                 <div className="form-group">
                                     <label>Token Supply</label>
                                     <div className="filter-widget">
                                         <input
                                             type="number"
+                                            placeholder="Enter Total Supply"
                                             required
-                                            value={tokens}
+                                            value={tokenSupply}
                                             placeholder=""
                                             className="form-control"
                                             onChange={(e) => {
-                                                setTokens(e.target.value);
+                                                setTokenSupply(e.target.value);
                                             }}
                                         />
                                     </div>
                                 </div>
-
+                                <label>Select Supply Type</label>
+                                <div className="filter-widget">
+                                    <Autocomplete
+                                        id="combo-dox-demo"
+                                        required
+                                        options={supplies}
+                                        // disabled={isDisabledImporter}
+                                        getOptionLabel={(option) =>
+                                            option
+                                        }
+                                        onChange={(event, value) => {
+                                            if (value == null) setType("");
+                                            else {
+                                                console.log(value);
+                                                setSupply(value)
+                                            }
+                                        }}
+                                        renderInput={(params) => (
+                                            <TextField
+                                                {...params}
+                                                label="Supply Type"
+                                                variant="outlined"
+                                            />
+                                        )}
+                                    />
+                                </div>
 
                                 <div className="form-group">
+                                    <label>Title</label>
                                     <div className="form-group">
                                         <input
                                             type="text"
                                             required
-                                            value={category}
+                                            value={title}
                                             placeholder="Enter Art Work Title"
                                             className="form-control"
                                             onChange={(e) => {
-                                                setCategory(e.target.value)
+                                                setTitle(e.target.value)
                                             }}
                                         />
                                     </div>
                                     <div className="form-group">
+                                        <label>Description</label>
                                         <textarea
                                             type="text"
                                             required
@@ -358,12 +469,12 @@ function NewNFT(props) {
                                             required
                                             type="file"
                                             name="sampleFile"
-                                            accept=".jpg,.png,.jpeg,.jfif,.pdf,.docx"
+                                            accept=".mp3"
                                             className="form-control"
-                                            onChange={(e) => fileSelectHandler(e)}
+                                            onChange={(e) => uploadMusicHandler(e)}
                                         />
                                     </div>
-                                    {category === '' || description === '' || tokens === '' || fileData === '' || importerId === "" || typeof importerId === "undefined" ? (
+                                    {/* {title === '' || description === '' || tokenSupply === '' || fileData === '' || setType === "" || typeof setType === "undefined" ? (
                                         <button
                                             className="btn"
                                             type="submit"
@@ -379,7 +490,7 @@ function NewNFT(props) {
                                         >
                                             <i className="fa fa-upload"></i>{' '} Upload to IPFS
                                         </button>
-                                    )}
+                                    )} */}
                                 </div>
                             </div>
                         </form>
@@ -413,19 +524,19 @@ function NewNFT(props) {
                         </Spinner>
                     </div>
                 ) : (
-                    inputList.length === 0 ? (
-                        <div className="submit-section">
-                            <button type="button" disabled className="btn submit-btn">
-                                Create NFT
-                    </button>
-                        </div>
-                    ) : (
-                        <div className="submit-section">
-                            <button type="button" onClick={handleSubmitEvent} className="btn submit-btn">
-                                Create NFT
+                    // inputList.length === 0 ? (
+                    //     <div className="submit-section">
+                    //         <button type="button" disabled className="btn submit-btn">
+                    //             Create NFT
+                    // </button>
+                    //     </div>
+                    // ) : (
+                    <div className="submit-section">
+                        <button type="button" onClick={handleSubmitEvent} className="btn submit-btn">
+                            Create NFT
                   </button>
-                        </div>
-                    )
+                    </div>
+                    // )
 
                 )}
             </div>
