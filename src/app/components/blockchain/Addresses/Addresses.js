f79b2474178c7c88c77399bc50a00e3ab5898481
adreees0512133526 <muhammad.adrees@scytalelabs.com>
2021-05-19 17:04:50 +0500

    Smart Contract Addresses updated

--- src/app/components/blockchain/Addresses/Addresses.js
+++ src/app/components/blockchain/Addresses/Addresses.js
@@ -1,6 +1,5 @@
-export const USDTTokenAddress = '0xd60c953142753c1339e5686d48256a90F59e53CA';
-export const CreateNftAddress = '0xc361a3EaDa3C6cc606E1709d81d53E7f36da982f';
-export const CreateCubeAddress = '0x8B805eA292A46D2F46c2a3DC626C4bEd6Eae575A';
-export const AuctionAddress = "0x612757c1424df91D61C82a412835A2977f7bD493";
-export const WethAddress = "0xd88aA5d70b97D70993E19Fa45949C8F37796CDF2";
-export const MarketPlaceAddress = "0xcE01D67Ea1106925d3d79aa947c83fCc21233Cc8";
+export const CreateNftAddress = '0x9B2D957D6Da0477be3d7070F30F7524E69976A0d';
+export const CreateCubeAddress = '0x2fC35aEDBC3d47837F24A746D67782fe9c87C0E8';
+export const AuctionAddress = "0x21A6256e5BB6Fe4f83F76A9701402ed555bDCFac";
+export const WethAddress = "0x0a180a76e4466bf68a7f86fb029bed3cccfaaac5";
+export const MarketPlaceAddress = "0x89c91582cD0098449E066eB5A31fC10986eACBB1";
