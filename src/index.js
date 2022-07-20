2bbd5f16bfd1d979cf81139f54c66d01a76a2d65
adrees0512133526 <muhammad.adrees@scytalelabs.com>
2022-07-20 19:28:10 +0500

    Backend Link Updated for Info

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ import axios from "axios";
 if (process.env.REACT_APP_BACKEND_SERVER_ADDRESS)
   axios.defaults.baseURL = `${process.env.REACT_APP_BACKEND_SERVER_ADDRESS}`;
 // else axios.defaults.baseURL = `http://localhost:3000`;
-else axios.defaults.baseURL = `https://api.casperswap.xyz/`;
+else axios.defaults.baseURL = `https://graphqlbackendkafka-env.eba-td22di2h.us-east-1.elasticbeanstalk.com/`;
+
 
 
 ReactDOM.render(<App />, document.getElementById("root"));
