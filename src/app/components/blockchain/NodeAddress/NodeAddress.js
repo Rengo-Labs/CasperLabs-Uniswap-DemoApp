0ded91bbcd62cc1273ab906cf5af34b9eb317e2c
adrees0512133526 <muhammad.adrees@scytalelabs.com>
2022-08-31 17:20:37 +0500

    Node Address Updated

--- src/app/components/blockchain/NodeAddress/NodeAddress.js
+++ src/app/components/blockchain/NodeAddress/NodeAddress.js
@@ -1,1 +1,1 @@
-export const NODE_ADDRESS = 'https://casperlabs-proxy-server.herokuapp.com/http://159.65.118.250:7777/rpc'
+export const NODE_ADDRESS = 'https://casperlabs-proxy-server.herokuapp.com/http://44.208.234.65:7777/rpc'
